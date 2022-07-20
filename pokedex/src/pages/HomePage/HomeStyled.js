cccd1b1146312b345224ed851b9a24ae250684aa
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-07-20 19:42:58 -0300

    Correções da estilização

--- pokedex/src/pages/HomePage/HomeStyled.js
+++ pokedex/src/pages/HomePage/HomeStyled.js
@@ -1,1 +1,10 @@
 import styled from 'styled-components'
+
+export const ContainerGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 2em 2em;
+  width: 100%;
+  margin: 2%;
+`
