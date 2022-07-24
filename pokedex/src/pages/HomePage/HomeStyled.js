36475d203f0deaac08eab39a51337efce6c8ca77
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-07-24 11:03:25 -0300

    Implementação do Loading

--- pokedex/src/pages/HomePage/HomeStyled.js
+++ pokedex/src/pages/HomePage/HomeStyled.js
@@ -4,7 +4,7 @@ export const ContainerGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 2em 2em;
+  gap: 2em 1em;
   width: 100%;
   margin: 2%;
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
